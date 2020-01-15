1a996995427bcbbee84c4496ba4256b8571a44e7
Henry O. Zorrilla <thmthmnky@gmail.com>
2020-01-15 14:07:53 -0800

    Adding sample s'svg' drawing before creating actuall showcase artwork

--- server.js
+++ server.js
@@ -8,17 +8,11 @@ const router = express.Router();
 app.use(express.static('public'));
 
 router.get('/',function(req,res){
-  // res.send('App works!!!!!');
   res.sendFile(path.join(__dirname, 'client', 'index.html'));
-  //__dirname : It will resolve to your project folder.
 });
 
 // app.use('/api', require('./routes'));
 app.use('/', router);
-
-app.get('*', (req, res) => {
-  res.status(200).send('App works!!!!!');
-});
 
 // 404: Not found
 app.use(function(req, res, next){
@@ -31,7 +25,7 @@ app.use(function(err, req, res, next){
     res.status(500).json({ERROR: 'Internal server error.'});
 });
 
-const port = process.env.port? process.env.port:3570;
+const port = process.env.port? process.env.port:3575;
 app.listen(port, (err) => {
   if (err) {
     console.error(`Error::${err}`);
@@ -40,4 +34,4 @@ app.listen(port, (err) => {
   console.info(`Server running on port::${port}`);
   // logger.info(`Server running on port::${port}`);
 });
-console.log('Running at Port 3570');
+console.log('Running at Port'+port);
